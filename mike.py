05ee15e1f389b8056ebf594c24835a827f04c446
24on7store <132136928+24on7store@users.noreply.github.com>
2023-09-19 15:52:47 +0300

    a try

---
+++ mike.py
@@ -0,0 +1,21 @@
+print()
+print("**********PART I Introduction (30 mn)**********")
+print()
+#1
+print("Introduction/welcome members: Missaindelee Pamphile")
+print()
+alloted_time = 2
+time_being_used = 2.40
+#difference = alloted_time_for_welcoming_guests - time_being_used
+extra_time = time_being_used - alloted_time
+
+print(f"alloted_time = {alloted_time}")
+
+print(f"Time being used = {time_being_used} ")
+print()
+if extra_time <= 0.5 or 0:
+   print(f"Congratulations, Was On time, Minutes being used = {time_being_used}")
+   print()
+else:
+    print(f"Unfortunately, Went Over time,  extra minutes used = {extra_time} ")
+print()
